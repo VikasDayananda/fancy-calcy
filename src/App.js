d1a5b8836ea5b433e43f51bf618b2f90c211bc31
VikasDayananda <vikasdayananda@gmail.com>
2018-10-23 23:38:06 -0700

    Map function added for cells

--- src/App.js
+++ src/App.js
@@ -1,83 +1,45 @@
-import React,{Component} from 'react'
-import { hot } from 'react-hot-loader'
-import { Container, Row, Col } from 'react-grid-system';
+import React, {Component} from 'react'
+import {hot} from 'react-hot-loader'
+import {Container, Row, Col} from 'react-grid-system';
 import './App.css'
-class App extends Component{
-    render(){
-        return(
-            <container className="container">
-                <Row className="result">
-                    <span>Result</span>
-                </Row>
-                <Row>
-                    <Col sm={3} className="cell">
-                        <span>AC</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>+/-</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>%</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>÷</span>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={3} className="cell">
-                        <span>7</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>8</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>9</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>×</span>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={3} className="cell">
-                        <span>4</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>5</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>6</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>-</span>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={3} className="cell">
-                        <span>1</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>2</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>3</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>+</span>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={6} className="cell">
-                        <span>0</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>.</span>
-                    </Col>
-                    <Col sm={3} className="cell">
-                        <span>=</span>
-                    </Col>
-                </Row>
 
-            </container>
+const obj = [
+    ['AC', '+/-', '%', '÷'],
+    ['7', '8', '9', '*'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '.', '=']
+
+]
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            value: 0
+        }
+    }
+
+    enteredValue(val) {
+        this.setState({value: val})
+    };
+
+    render() {
+        return (
+            <Container className="container">
+                <Row className="result">
+                    <span>{this.state.value}</span>
+                </Row>
+                {obj.map(val => {
+                    return <Row>
+                        {val.map(v => {
+                            return <Col sm={3} className="cell" onClick={() => this.enteredValue(v)}>
+                                <span>{v}</span>
+                            </Col>
+                        })}
+                    </Row>
+                })}
+            </Container>
         )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,9 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                use: ['babel-loader']
             },
             {
                 test: /\.css$/,
